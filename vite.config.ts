d1d81994c89c07e6fed210f7cab016fe3dc2ea15
Vajahath Ahmed <vajuoff.1@gmail.com>
2026-03-08 19:00:35 +0530

    ci

---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    /**
+     * For deploying to GitHub Pages as a subfolder, 
+     * we must set the base path correctly.
+     */
+    base: '/merry-christmas-ajesh/',
+});
